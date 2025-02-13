281490c8e2210bb67183deceb996fa6ede9922e3
ShveikOff <55891476+ShveikOff@users.noreply.github.com>
2025-02-13 15:06:58 +0600

    Файлы candle.h и candle.cpp включены в проект

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <functional>
 #include <iostream>
 
-#include "tests.h"
+#include "candle.h"
+
+// #include "tests.h"
 
 //массив всех тестов, который мы заполняем в функции initTests
 static std::vector<std::function<bool()>> tests;
